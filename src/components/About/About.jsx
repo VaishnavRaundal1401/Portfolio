e486df8a10f8481967a709e26992eff6af3db2e9
Vaishnav Raundal <vaishnavraundal2016@gmail.com>
2024-07-05 23:55:39 +0530

    changed the contact section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -28,7 +28,7 @@ const About = () => {
             animate={isInView && "animate"}
             >
                 <motion.div className="myInfoContainer" variants={variants}>
-                  <motion.h1 variants={variants}>Hello, My name is Vaishnav Raundal</motion.h1>
+                  <motion.h1 variants={variants}>Hello, How are you 😊</motion.h1>
                   <motion.span variants={variants}>
                   Welcome to my portfolio! My name is Vaishnav Popat Raundal and I'm from Nashik. Professionally, a Full Stack Developer, learning and working in IT field since 2021. I'm a passionate and dedicated Web Developer who loves to develop applications in MERN stack and i'm currently pursuing my Bachelor of Technology in Computer Science at PCCOE, Pune. As a upcomming Software Developer and, I strive to create impactful and innovative Software Products.
                   </motion.span>
@@ -36,17 +36,17 @@ const About = () => {
                     <motion.a href="/resume.pdf" download><motion.button variants={variants}>My Resume</motion.button></motion.a>
                   </motion.div>
                 </motion.div>
-
+                
                 <motion.div className="education">
+                  <motion.h1 variants={variants}>Qualification</motion.h1>
                   <motion.span variants={variants}>
                   My educational journey has been both challenging and rewarding. I completed my Diploma in Information Technology from Sou. Venutai Chavan Polytechnic, Pune, with a stellar percentage of 88.88%. Currently, I am pursuing my B.Tech in Computer Science, where I have maintained a CGPA of 8.4/10.
                   </motion.span>
-                  <motion.h1 variants={variants}>Past Qualification</motion.h1>
                 </motion.div>
 
            </motion.div>
                 <div className='aimageContainer'>
-                  <img src='/images/about.jpg' alt='About Background' />
+                  {/* <img src='/images/about.jpg' alt='About Background' /> */}
                 </div>
       </div>
   )
