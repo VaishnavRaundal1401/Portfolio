ace04fc96e2e506e08276ef807687bf22ef82585
Vaishnav Raundal <vaishnavraundal2016@gmail.com>
2024-07-11 19:46:25 +0530

    Implemented Glowing mouse Effect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.scss';
+import {useEffect} from 'react'
 import About from './components/About/About';
 import Contact from './components/Contact/Contact';
 import HomePage from './components/HomePage/HomePage';
@@ -6,6 +7,34 @@ import Navbar from './components/Navbar/Navbar';
 import Projects from './components/Projects/Projects';
 import Skills from './components/Skills/Skills';
 function App() {
+
+  useEffect(() => {
+    const handleOnMouse = e => {
+      const { currentTarget: target } = e;
+
+      const rect = target.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+
+      target.style.setProperty("--mouse-x", `${x}px`);
+      target.style.setProperty("--mouse-y", `${y}px`);
+    };
+
+    const sections = document.querySelectorAll("section");
+
+    sections.forEach(section => {
+      section.addEventListener('mousemove', handleOnMouse);
+    });
+
+    // Cleanup function to remove the event listener when the component unmounts
+    return () => {
+      sections.forEach(section => {
+        section.removeEventListener('mousemove', handleOnMouse);
+      });
+    };
+
+  }, []);
+
   return (
     <div>
     <section id="HomePage">
